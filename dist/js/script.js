819e43325e476ccb0f12087d90958250b4e12f2c
Azwar-as <azwaranshari08@gmail.com>
2024-08-21 22:55:40 +0700

    First Commit

---
+++ dist/js/script.js
@@ -0,0 +1,53 @@
+// Navbar Fixed
+window.onscroll = function () {
+    const header = document.querySelector('header')
+    const fixedNav = header.offsetTop;
+    const toTop = document.getElementById("toTop")
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
+
+    } else {
+        header.classList.remove('navbar-fixed')
+        toTop.classList.remove('flex')
+        toTop.classList.add('hidden')
+    }
+}
+
+//Hamburger
+const hamburger = document.querySelector("#hamburger")
+const navMenu = document.querySelector('#nav-menu')
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('hamburger-active')
+    navMenu.classList.toggle('hidden')
+})
+
+// klik diluar hamburger
+window.addEventListener('click', (e) => {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden')
+    }
+})
+
+// darkmode Toggle
+const darkToggle = document.getElementById("dark-toggle")
+const html = document.querySelector('html')
+
+darkToggle.addEventListener('click', () => {
+    if (darkToggle.checked) {
+      html.classList.add("dark")
+        localStorage.theme = "dark"
+    } else {
+         html.classList.remove("dark")
+        localStorage.theme = "light"
+    }
+})
+
+//Toggle swap
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true
+} else {
+    darkToggle.checked = false
+}

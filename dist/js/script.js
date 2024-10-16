1a69e365368cf9bfcb3d79a986062fe68b5fb068
https://github.com/Azwar-as/MrAzwar.github.io.git <azwaranshari08@gmail.com>
2024-10-16 09:02:43 +0700

    modified interface website

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,115 +1,55 @@
-<<<<<<< HEAD
 // Navbar Fixed
 window.onscroll = function () {
-    const header = document.querySelector('header')
-    const fixedNav = header.offsetTop;
-    const toTop = document.getElementById("toTop")
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed')
-        toTop.classList.remove('hidden')
-        toTop.classList.add('flex')
-
-    } else {
-        header.classList.remove('navbar-fixed')
-        toTop.classList.remove('flex')
-        toTop.classList.add('hidden')
-    }
-}
-
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.getElementById("toTop");
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
 //Hamburger
-const hamburger = document.querySelector("#hamburger")
-const navMenu = document.querySelector('#nav-menu')
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
-})
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
 // klik diluar hamburger
-window.addEventListener('click', (e) => {
-    if (e.target != hamburger && e.target != navMenu) {
-        hamburger.classList.remove('hamburger-active')
-        navMenu.classList.add('hidden')
-    }
-})
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
 
 // darkmode Toggle
-const darkToggle = document.getElementById("dark-toggle")
-const html = document.querySelector('html')
+const darkToggle = document.getElementById("dark-toggle");
+const html = document.querySelector("html");
 
-darkToggle.addEventListener('click', () => {
-    if (darkToggle.checked) {
-<<<<<<< HEAD
-      html.classList.add("dark")
-        localStorage.theme = "dark"
-    } else {
-         html.classList.remove("dark")
-=======
-        html.classList.add("dark")
-        localStorage.theme = "dark"
-    } else {
-        html.classList.remove("dark")
->>>>>>> 405c042 (first commit)
-        localStorage.theme = "light"
-    }
-})
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked) {
+    localStorage.theme = "dark";
+    html.classList.add("dark");
+  } else {
+    localStorage.theme = "light";
+    html.classList.remove("dark");
+  }
+});
 
 //Toggle swap
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    darkToggle.checked = true
-} else {
-    darkToggle.checked = false
-=======
-// Navbar Fixed
-window.onscroll = function () {
-    const header = document.querySelector('header')
-    const fixedNav = header.offsetTop;
-    const toTop = document.getElementById("toTop")
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed')
-        toTop.classList.remove('hidden')
-        toTop.classList.add('flex')
-
-    } else {
-        header.classList.remove('navbar-fixed')
-        toTop.classList.remove('flex')
-        toTop.classList.add('hidden')
-    }
-}
-
-//Hamburger
-const hamburger = document.querySelector("#hamburger")
-const navMenu = document.querySelector('#nav-menu')
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
-})
-
-// klik diluar hamburger
-window.addEventListener('click', (e) => {
-    if (e.target != hamburger && e.target != navMenu) {
-        hamburger.classList.remove('hamburger-active')
-        navMenu.classList.add('hidden')
-    }
-})
-
-// darkmode Toggle
-const darkToggle = document.getElementById("dark-toggle")
-const html = document.querySelector('html')
-
-darkToggle.addEventListener('click', () => {
-    if (darkToggle.checked) {
-        html.classList.add("dark")
-        localStorage.theme = "dark"
-    } else {
-        html.classList.remove("dark")
-        localStorage.theme = "light"
-    }
-})
-
-//Toggle swap
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    darkToggle.checked = true
-} else {
-    darkToggle.checked = false
->>>>>>> 9758282 (revision)
-}
+// if (
+//   localStorage.theme === "dark" ||
+//   (!("theme" in localStorage) &&
+//     window.matchMedia("(prefers-color-scheme: dark)").matches)
+// ) {
+//   darkToggle.checked = true;
+// } else {
+//   darkToggle.checked = false;
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
         primary: "#22c55e",
         dark: "#0f172a",
-        secondary: '#64748b',
-        light: '#ffff'
+        secondary: "#64748b",
+        light: "#ffff",
       },
       screens: {
-        '2xl': '1320px',
-
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
-
+};
